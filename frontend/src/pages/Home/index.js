8678e0a39b62bf64e8b530e92546213ea122ced4
Maycon <m.vinnicyus@gmail.com>
2020-08-04 20:55:12 -0300

    feat: api configs

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../config/api';
 
 import Header from '../../components/Header';
 import './style.scss';
 
 export default () => {
+  const [popular, setPopular] = useState({});
+
+  useEffect(async () => {
+    const data = await api.get('/movies/popular').then(res => res.data);
+    setPopular(data);
+  }, []);
+
   return (
     <>
       <Header />
