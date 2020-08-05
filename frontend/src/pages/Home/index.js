d87aecb5812057ede72b5bdfeb59b4c78f9295d6
Maycon <m.vinnicyus@gmail.com>
2020-08-04 21:49:26 -0300

    feat: html carrousel base structure

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react';
 import api from '../../config/api';
 
 import Header from '../../components/Header';
-import './style.scss';
+import styles from './style.module.scss';
 
 export default () => {
-  const [popular, setPopular] = useState({});
+  const [popular, setPopular] = useState({ results: [] });
 
   useEffect(async () => {
     const data = await api.get('/movies/popular').then(res => res.data);
@@ -15,7 +15,28 @@ export default () => {
   return (
     <>
       <Header />
-      <h1>Home</h1>
+      <main>
+        <section className={styles.carrousel}>
+          <div className={styles.trackContainer}>
+            <ul className={styles.track}>
+              {popular.results.map((movie, index) => (
+                <li
+                  className={`${styles.slide} ${
+                    index === 0 ? styles.currentSlide : ''
+                  }`}
+                >
+                  <img
+                    className={styles.image}
+                    key={movie.id}
+                    src={movie.backdrop_path}
+                    alt={movie.title}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+      </main>
     </>
   );
 };
