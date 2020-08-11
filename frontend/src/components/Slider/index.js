4307a72068ad3a4bce3165f57df3d6ce7504fa3d
Maycon <m.vinnicyus@gmail.com>
2020-08-11 20:57:37 -0300

    style: slider title underline with after

--- frontend/src/components/Slider/index.js
+++ frontend/src/components/Slider/index.js
@@ -5,7 +5,9 @@ import styles from './styles.module.scss';
 export default function Slider({ title, children, height = '150px' }) {
   return (
     <div>
-      <h2 className={styles.title}>{title}</h2>
+      <h2 className={styles.title}>
+        <span>{title}</span>
+      </h2>
 
       <div style={{ height }} className={styles.sliderContainer}>
         {children}
