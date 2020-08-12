b7e60c2a514694b6a5bdbfbacb5ab57153ff5e38
Maycon <m.vinnicyus@gmail.com>
2020-08-12 16:56:27 -0300

    feat: react router dom applied

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Home from './pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './styles/global.scss';
 
 export default () => {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route component={Home} path="/" exact />
+      </Switch>
+    </BrowserRouter>
   );
 };
