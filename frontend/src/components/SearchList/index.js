6608a8413bf79e8dd7ed50493f14523b964131f9
Maycon <m.vinnicyus@gmail.com>
2020-08-14 19:28:07 -0300

    feat: loading component on load

--- frontend/src/components/SearchList/index.js
+++ frontend/src/components/SearchList/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import styles from './styles.module.scss';
+import Loading from '../Loading';
 
 export default function SearchList({ children }) {
+  if (!children) return <Loading />;
   return <div className={styles.container}>{children}</div>;
 }
