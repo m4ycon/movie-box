0ad2e418431f3cde65e4b4bdbedbf82ec5853f07
Maycon Fabio <m.vinnicyus@gmail.com>
2021-07-11 17:57:01 -0300

    feat: init a react project with TS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,6 @@
+
+function App() {
+  return <h1>HOME</h1>
+}
+
+export default App
--- frontend/src/components/SearchList/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import styles from './styles.module.scss';
-import Loading from '../Loading';
-
-export default function SearchList({ children }) {
-  if (!children) return <Loading />;
-  return <div className={styles.container}>{children}</div>;
-}
